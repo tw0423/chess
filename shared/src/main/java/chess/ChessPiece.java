07b4d8f7363076634d944babe7240b645a44c239
tw0423 <tw0423@byu.edu>
2025-01-23 16:22:46 -0700

    git push

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,9 +11,14 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
+
 
     /**
      * The various different chess piece options
@@ -29,14 +36,30 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -47,6 +70,46 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        if (this.type == PieceType.PAWN){
+            PawnMovesCalculator PawnMoves = new PawnMovesCalculator(myPosition, board, this);
+            return PawnMoves.PawnMoves();
+        }
+        else if (this.type == PieceType.QUEEN){
+            QueenMovesCalculator QueenMoves = new QueenMovesCalculator(myPosition, board, this);
+            return QueenMoves.QueenMoves();
+        }
+        else if (this.type == PieceType.KING){
+            KingMovesCalculator KingMoves = new KingMovesCalculator(myPosition, board, this);
+            return KingMoves.KingMoves();
+        }
+        else if (this.type == PieceType.BISHOP){
+            BishopMovesCalculator BishopMoves = new BishopMovesCalculator(myPosition, board, this);
+            return BishopMoves.BishopMoves();
+        }
+        else if (this.type == PieceType.ROOK){
+            RookMovesCalculator RookMoves = new RookMovesCalculator(myPosition, board, this);
+            return RookMoves.rookMoves();
+        }
+        else if (this.type == PieceType.KNIGHT){
+            KnightMovesCalculator KnightMoves = new KnightMovesCalculator(myPosition, board, this);
+            return KnightMoves.KnightMoves();
+        }
+
+
+//        else if (this.type == PieceType.KNIGHT){
+//            return
+//        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 }
+
+
+
