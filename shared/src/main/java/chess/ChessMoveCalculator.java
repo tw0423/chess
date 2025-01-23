2e5586c66977892651e1770958cd0682ef36d277
tw0423 <tw0423@byu.edu>
2025-01-23 16:20:44 -0700

    git push

--- shared/src/main/java/chess/ChessMoveCalculator.java
+++ shared/src/main/java/chess/ChessMoveCalculator.java
@@ -6,16 +6,27 @@ import java.util.Collection;
 public class ChessMoveCalculator{
     private final ChessPosition myPosition;
     private final ChessBoard myBoard;
+    private final Collection<ChessMove> myMoves;
+    private final ChessPiece myChessPiece;
 
-    public ChessMoveCalculator(ChessPosition position, ChessBoard board) {
-        myPosition = position;
-        myBoard = board;
+
+
+    public ChessMoveCalculator(ChessPosition position, ChessBoard board, ChessPiece piece) {
+        this.myPosition = position;
+        this.myBoard = board;
+        var type = myBoard.getPiece(myPosition);
+        this.myMoves = new ArrayList<>();
+        this.myChessPiece = piece;
     }
 
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>();
-        return moves;
+
+
+        return myMoves;
     }
+
+
+
 
 
 }
