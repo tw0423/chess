fb3b3005a0d24b762d37590dada062e79d581bc3
tw0423 <tw0423@byu.edu>
2025-02-07 03:48:37 -0700

    git push

---
+++ shared/src/main/java/chess/KnightMoveCalculator.java
@@ -0,0 +1,46 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KnightMoveCalculator extends ChessMoveCalculator {
+    private int row;
+    private int col;
+
+    public KnightMoveCalculator(ChessBoard board, ChessPosition position) {
+        super(board, position);
+        this.row = position.getRow();
+        this.col = position.getColumn();
+    }
+
+    public Collection<ChessMove> KnightMoves(){
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        int [][] directions = {
+                {2, 1},
+                {1, 2},
+                {-1, 2},
+                {-2 ,1},
+                {-2, -1},
+                {-1, -2},
+                {1, -2},
+                {2, -1}
+        };
+        for (int[] direction : directions) {
+            int MovingRow = row + direction[0];
+            int MovingCol = col + direction[1];
+            ChessPosition MovingPosition = new ChessPosition(MovingRow, MovingCol);
+            if (MovingRow < 1 || MovingRow > 8 || MovingCol < 1 || MovingCol > 8) {
+                continue;
+            }
+            else if (this.board.empty(MovingPosition) || this.board.notSameTeam(this.position, MovingPosition))
+            {
+                moves.add(new ChessMove(this.position, MovingPosition, null));
+            }
+
+
+        }
+        return moves;
+
+    }
+}
