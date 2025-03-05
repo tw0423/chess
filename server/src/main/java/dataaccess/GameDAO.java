26c14318a3dd02c4909acf301ede271dcdaef824
backupMac <tzulee0423@gmail.com>
2025-03-05 14:57:27 -0700

    git push

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -6,10 +6,10 @@ public interface GameDAO {
 
     ArrayList<GameData> listGames();
     void createGame(GameData game);
-    GameData getGame(int gameID) throws EmptyDataException, AccessDeniedException;
-    boolean ExistGame(int gameID) throws EmptyDataException, AccessDeniedException;
+    GameData getGame(int gameID)  throws DataAccessException;
+    boolean ExistGame(int gameID)  throws DataAccessException;
 
-    void updateGame(GameData game) throws AccessDeniedException;
+    void updateGame(GameData game)  throws DataAccessException;
     void clear();
 }
 
