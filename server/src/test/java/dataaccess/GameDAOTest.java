2e4f69a5d2aff184af09b9de5b5414ffc3cc5851
backupMac <tzulee0423@gmail.com>
2025-03-13 17:00:02 -0600

    fixed the unit test

--- server/src/test/java/dataaccess/GameDAOTest.java
+++ server/src/test/java/dataaccess/GameDAOTest.java
@@ -1,4 +1,50 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class GameDAOTest {
+
+    GameDAO gameDAO;
+
+    {
+        try {
+            gameDAO = new SQLGameDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    private UserData volunteer1 = new UserData("volunteer1","abcde","volunteer1@gmail.com");
+    private GameData game1 = new GameData(1, null, null, "game1", new ChessGame());
+    private GameData repeatGame = new GameData(1, null, null, "game1", new ChessGame());
+
+    private AuthData badAuth = new AuthData("volunteer2",null);
+
+    @Test
+    public void testPositiveCreatGames() throws SQLException {
+        try {
+            gameDAO.createGame(game1);
+            ArrayList<GameData> lists = gameDAO.listGames();
+
+            Assertions.assertEquals(1, lists.size());
+
+        } catch (DataAccessException e) {
+            assert false;
+        }
+    }
+
+
+
+
+
 }
